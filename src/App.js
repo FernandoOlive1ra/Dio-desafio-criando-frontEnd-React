6204d1b9d7226555f246af617352709bb7db577f
fernando <fernandool2@hotmail.com>
2022-07-22 16:03:57 -0300

    Parte 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React from "react";
+import Layout from "./components/layout";
 
 function App() {
   return (
-    <div className="App">
-      GitHub API
-    </div>
+    <main>
+      <Layout>
+          <div>
+            <img src="https://avatars.githubusercontent.com/u/105995190?v=4" alt="avatar of user"/>
+            <h1>Fernando Oliveira</h1>
+            <h3>Username: </h3>
+            <span>FernandoOlive1ra</span>
+          <div>
+            <div>
+              <h4>Followers</h4>
+              <span>5</span>
+            </div>
+            <div>
+              <h4>Starrds</h4>
+              <span>5</span>
+            </div>
+            <div>
+              <h4>Followings</h4>
+              <span>5</span>
+            </div>
+          </div>
+          
+
+          </div>
+          <div>Repositories</div>
+          <div>Starrds</div>
+      </Layout>
+    </main>
   );
 }
 
---
+++ src/services/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+
+const api =axios.create({
+    baseUrl: 'https://api.github.com'
+})
+
+export default api;
+
